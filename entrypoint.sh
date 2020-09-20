2873154f8ab981c816470fa4c2fc19e50be381ea
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-09-20 19:00:56 +0200

    Fix substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -113,7 +113,7 @@ echo "Creating commit"
 git add "${TARGET_PATH}" || exit 1
 git restore --staged changelog.md
 git commit -m "${COMMIT_MESSAGE}" --author "${COMMIT_AUTHOR}" || exit 0
-echo "Created commit ${git rev-parse HEAD}"
+echo "Created commit $(git rev-parse HEAD)"
 npx gitmoji-changelog@canary --preset generic --output changelog.md
 git add changelog.md
 git commit -m ":bookmark: Update changelog" --author "${COMMIT_AUTHOR}" || exit 0
